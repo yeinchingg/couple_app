4913c9cac19bf16ba8dd3b34444dd222c066160f
yanxing <qwe33370@gmail.com>
2025-06-17 16:01:52 +0800

    add navigate

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
 import 'package:couple_app/theme/text_styles.dart';
-import 'package:couple_app/theme/button_styles.dart';
 import 'package:couple_app/theme/color_styles.dart';
 import 'package:couple_app/chat/chat_page.dart';
-
+import 'package:couple_app/ widgets/navigate.dart';
+import 'package:couple_app/screens/calender.dart';
+import 'package:couple_app/screens/photo.dart';
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -11,66 +12,93 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-int _count = 0;
 
 class _ProfilePageState extends State<ProfilePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.backgroundcolor,
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'couple app ',
-            textAlign: TextAlign.center,
-            style: bodyStyle,
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.chat_bubble_outline, color: AppColors.Dgreen),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ChatPage()),
-              );
-            },
-          ),
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          SizedBox(height: 40),
-          Text("XXX Day ", style: headingStyle),
-          SizedBox(height: 40),
+  int _selectedIndex = 0;
+  int _count = 0;
 
-          Row(
-            children: [
-              // Container(
-              // padding: EdgeInsets.all(16),
-              // decoration: BoxDecoration(
-              //   color: Colors.teal[100],
-              //   borderRadius: BorderRadius.circular(12),
-              // ),
-              InkWell(
-                onTap: () => setState(() => _count++),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Image.asset(
-                    'lib/assets/flower.gif', // 確保這張圖在 pubspec.yaml 有註冊
-                    width: 300,
-                    height: 550,
-                    fit: BoxFit.cover,
+    void _onItemTapped(int index) {
+      if (index == _selectedIndex) return;
+    setState(() {
+      _selectedIndex = index;
+    });
+      switch (index) {
+        case 0:
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => CalendarMemoPage()),
+          );
+          break;
+        case 1:
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) =>ProfilePage() ),
+          );
+          break;
+        case 2:
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => PhotoWallPage()),
+          );
+          break;
+      }
+    }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: AppColors.backgroundcolor,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'couple app ',
+              textAlign: TextAlign.center,
+              style: bodyStyle,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.chat_bubble_outline, color: AppColors.Dgreen),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChatPage()),
+                );
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            SizedBox(height: 40),
+            Text("XXX Day ", style: headingStyle),
+            SizedBox(height: 40),
+
+            Row(
+              children: [
+                InkWell(
+                  onTap: () => setState(() => _count++),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.asset(
+                      'lib/assets/flower.gif', // 確保這張圖在 pubspec.yaml 有註冊
+                      width: 300,
+                      height: 550,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ),
-              // ),
-              SizedBox(width: 8),
-              Text(" +$_count !", style: bodyStyle),
-            ],
-          ),
-        ],
-      ),
-    );
+                // ),
+                SizedBox(width: 8),
+                Text(" +$_count !", style: bodyStyle),
+              ],
+            ),
+            SizedBox(height: 40),
+          ],
+        ),
+        bottomNavigationBar: CustomNavigationBar(
+          selectedIndex: _selectedIndex,
+          onItemTapped: _onItemTapped,
+        ),
+      );
+    }
   }
-}
